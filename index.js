6912000d458de0d8dac638d07d8d379bb116c81b
Raynos <raynos2@gmail.com>
2014-03-30 21:59:24 -0700

    initial version

---
+++ index.js
@@ -0,0 +1,58 @@
+var raf = require("raf").polyfill
+var vdomCreate = require("virtual-dom/render")
+var vdomDiff = require("virtual-dom/diff")
+var vdomPatch = require("virtual-dom/patch")
+
+module.exports = main
+
+function main(initialState, view, opts) {
+    opts = opts || {}
+
+    var currentState = initialState
+    var create = opts.create || vdomCreate
+    var diff = opts.diff || vdomDiff
+    var patch = opts.patch || vdomPatch
+    var looping = true
+
+    var tree = view(currentState)
+    var target = create(tree, opts)
+
+    currentState = null
+
+    raf(redraw)
+
+    return {
+        target: target,
+        update: update
+    }
+
+    function update(state) {
+        if (currentState === null && !looping) {
+            looping = true
+            raf(redraw)
+        }
+
+        currentState = state
+    }
+
+    function redraw() {
+        if (currentState === null) {
+            looping = false
+            return
+        }
+
+        var newTree = view(currentState)
+
+        if (opts.createOnly) {
+            create(newTree, opts)
+        } else {
+            var patches = diff(tree, newTree)
+            patch(target, patches)
+        }
+
+        tree = newTree
+        currentState = null
+
+        raf(redraw)
+    }
+}
---
+++ test/index.js
@@ -0,0 +1,67 @@
+var test = require("tape")
+var h = require("virtual-dom/h")
+var document = require("global/document")
+var raf = require("raf").polyfill
+
+var mainLoop = require("../index")
+
+test("mainLoop is a function", function (assert) {
+    assert.equal(typeof mainLoop, "function")
+    assert.end()
+})
+
+test("can set up main loop", function (assert) {
+    var initState = { fruits: ["apple", "banana"], name: "Steve" }
+
+    function render(state) {
+        return h("div", [
+            h("div", [
+                h("span", "hello "),
+                h("span.name", state.name)
+            ]),
+            h("ul", state.fruits.map(renderFruit))
+        ])
+
+        function renderFruit(fruitName) {
+            return h("li", [
+                h("span", fruitName)
+            ])
+        }
+    }
+
+    // set up a loop
+    var loop = mainLoop(initState, render, {
+        document: document
+    })
+    document.body.appendChild(loop.target)
+
+    var div = loop.target
+    var span = div.childNodes[0].childNodes[1]
+    var ul = div.childNodes[1]
+
+    assert.equal(div.tagName, "DIV")
+    assert.equal(span.childNodes[0].data, "Steve")
+    assert.equal(ul.childNodes.length, 2)
+
+    // update the loop with the new application state
+    loop.update({
+        fruits: ["apple", "banana", "cherry"],
+        name: "Steve"
+    })
+
+    raf(function () {
+        assert.equal(ul.childNodes.length, 3)
+
+        loop.update({
+            fruits: ["apple", "banana", "cherry"],
+            name: "Stevie"
+        })
+
+        raf(function () {
+            assert.equal(span.childNodes[0].data, "Stevie")
+
+            document.body.removeChild(loop.target)
+            assert.end()
+        })
+    })
+})
